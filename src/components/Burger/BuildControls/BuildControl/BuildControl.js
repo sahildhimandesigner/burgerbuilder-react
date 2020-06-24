ca0903c80e2bab4a6f7bcf2cab9c91f6abbc8b3d
your username <sahil@ucreate.co.in>
2020-06-25 00:38:11 +0530

    implemented the add ingredients in burgger application

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import './BuildControl'
+import './BuildControl.css'
 
 const BuildControl = (props) => {
     return (
         <div className="BuildControl">
             <div className="label">{props.label}</div>
             <button className="Less">Less</button>
-            <button className="More">More</button>
+            <button className="More" onClick={props.added}>More</button>
         </div>
     )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Aux from './../../hoc/Aux'
 import Burger from './../../components/Burger/Burger'
+import BurgerBuilderControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 
 class BurgerBuilder extends React.Component {
 
@@ -10,13 +18,35 @@ class BurgerBuilder extends React.Component {
             bacon: 0,
             chees: 0,
             meat: 0,
-        }
+        },
+        totalPrice: 4, 
+    }
+
+    appIngredientHandler = (type) => {
+        debugger;
+        const oldCount = this.state.ingredients[type]
+        const updateCounted = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updateCounted;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientsHandler = (type) => {
+        debugger;
     }
 
     render(){
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
+                <BurgerBuilderControls 
+                    ingredientsAdded={this.appIngredientHandler}
+                />
             </Aux>
         )
     }
